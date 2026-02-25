f711c170b2e6069bb5a9bf9c92eb38b90b209f26
tndyd123 <wjdtndyd0001@gmail.com>
2026-02-25 12:30:41 +0900

    sprint1 part1 완료

---
+++ src/main/java/com/sprint/mission/discodeit/run/JavaApplication.java
@@ -0,0 +1,68 @@
+package com.sprint.mission.discodeit.run;
+
+import com.sprint.mission.discodeit.entity.Domain.Channel;
+import com.sprint.mission.discodeit.entity.Domain.Message;
+import com.sprint.mission.discodeit.entity.Domain.User;
+import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
+import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
+import com.sprint.mission.discodeit.service.jcf.JCFUserService;
+
+import java.util.UUID;
+
+public class JavaApplication {
+    public static void main(String[] args) {
+        JCFChannelService channelService = new JCFChannelService();
+        JCFUserService userService = new JCFUserService();
+        JCFMessageService messageService = new JCFMessageService();
+        System.out.println("=====================userTEST===============================================");
+        UUID user1 = userService.create(new User("정수용", "아하라마", "ONLINE"));
+        UUID user2 = userService.create(new User("백승준", "seungman", "OFFLINE"));
+        System.out.println("전체 조회/생성 테스트");
+        System.out.println(userService.readAll());
+
+        System.out.println("user1 단건 조회");
+        System.out.println(userService.read(user1));
+
+        userService.update(user1, "정수용 업데이트", "아하라마 업데이트", "ONLINE");
+        System.out.println("업데이트 테스트");
+        System.out.println(userService.read(user1));
+
+        userService.delete(user2);
+        System.out.println("user2 삭제 테스트/ 전체 조회시 user2가 없어야함");
+        System.out.println(userService.readAll());
+
+        System.out.println("=====================channelTEST===============================================");
+        UUID channel1 = channelService.create(new Channel("정수용님의 채널", "aaaaaaaaaaaaa"));
+        UUID channel2 = channelService.create(new Channel("백승준님의 채널", "ccccccccccccc"));
+        System.out.println("전체 조회/생성 테스트");
+        System.out.println(userService.readAll());
+
+        System.out.println("channel1 단건 조회");
+        System.out.println(channelService.read(channel1));
+
+        channelService.update(channel1, "채널 업데이트", "채널주소 업데이트");
+        System.out.println("업데이트 테스트");
+        System.out.println(channelService.read(channel1));
+
+        channelService.delete(channel2);
+        System.out.println("channel2 삭제 테스트/ 전체 조회 시 channel2가 없어야함");
+        System.out.println(channelService.readAll());
+
+        System.out.println("=====================messageTEST===============================================");
+        UUID message1 = messageService.create(new Message("보내는 내용", "보내는 사람", "받는 사람"));
+        UUID message2 = messageService.create(new Message("send message", "sender", "receiver"));
+        System.out.println("전체 조회/생성 테스트");
+        System.out.println(messageService.readAll());
+
+        System.out.println("message1 단건 조회");
+        System.out.println(messageService.read(message1));
+
+        messageService.update(message1, "업데이트된 메세지");
+        System.out.println("업데이트 테스트");
+        System.out.println(messageService.read(message1));
+
+        userService.delete(message2);
+        System.out.println("message2 삭제 테스트/ 전체 조회시 message2가 없어야함");
+        System.out.println(userService.readAll());
+    }
+}
