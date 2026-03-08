7a28bdbb8c72d0c85706f369ca546804b21689f1
jeongjae5310 <jeongjae5310@gmail.com>
2026-03-08 22:37:38 +0900

    테스트 코드 수정

--- src/main/java/com/sprint/mission/discodeit/entity/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/entity/JavaApplication.java
@@ -19,15 +19,13 @@ import com.sprint.mission.discodeit.service.file.FileUserService;
 public class JavaApplication {
     public static void main(String[] args) {
 
-        UserRepository userRepository = new JCFUserRepository();
-        MessageRepository messageRepository = new JCFMessageRepository();
-        ChannelRepository channelRepository = new JCFChannelRepository();
-
+        UserRepository userRepository = new FileUserRepository();
+        MessageRepository messageRepository = new FileMessageRepository();
+        ChannelRepository channelRepository = new FileChannelRepository();
 
         BasicUserService userService = new BasicUserService(userRepository);
         BasicMessageService messageService = new BasicMessageService(messageRepository);
         BasicChannelService channelService = new BasicChannelService(channelRepository);
-
 
         userService.setMessageService(messageService);
         userService.setChannelService(channelService);
@@ -36,84 +34,82 @@ public class JavaApplication {
         messageService.setChannelService(channelService);
         messageService.setUserService(userService);
 
-        User user1 = new User("유저1","이메일1", "비번1");
-        userService.create(user1);
-        userService.create(user1);
-        System.out.println("\n===유저(ALL) 조회 테스트===");
-        userService.readAll().stream()
-                .forEach(System.out::println);
+        //#################################################
+        System.out.println("===채팅 서비스 테스트===\n");
 
-        Channel channel1 = new Channel("채널1", user1.getId());
-        channelService.create(channel1);
+        //유저 생성
 
-        Message message1 = new Message("메시지1 내용", channel1.getId(), user1.getId());
-        messageService.create(message1);
-
-        System.out.println("\n===메세지 조회 테스트===");
-        messageService.readAll().stream()
-                .forEach(System.out::println);
-
-    }
-}
-/*//유저 만들기(유저 1,2)
         User user1 = new User("유저1","이메일1", "비번1");
         userService.create(user1);
         User user2 = new User("유저2","이메일2", "비번2");
         userService.create(user2);
-        //유저 생성 확인
+        User user3 = new User("유저3","이메일3", "비번3");
+        userService.create(user3);
+        User user4 = new User("유저4","이메일4", "비번4");
+        userService.create(user4);
+        System.out.println("유저 생성 완료");
 
-        //채널 만들기(유저1이 어드민)
-        Channel channel1 = new Channel("채널1", user1.getId());
-        channelService.create(channel1);
-        Channel channel2 = new Channel("채널2", user2.getId());
-        channelService.create(channel2);
-        //채널 생성 확인
+        //#################################################
+        System.out.println("\n===유저 조회(단건) 테스트===\n");
 
-        System.out.println("\n===유저(ALL) 조회 테스트===");
+        System.out.println(userService.read(user1.getId()));
+
+        //#################################################
+        System.out.println("\n===유저 조회(전체) 테스트===\n");
+
         userService.readAll().stream()
                 .forEach(System.out::println);
 
-        System.out.println("\n===채널(ALL) 조회 테스트===");
-        channelService.readAll().stream()
-                .forEach(System.out::println);
-        System.out.println("\n유저1 속한 채널");
-        user1.getJoinedChannelId().stream()
-                .map(channelId -> channelService.read(channelId).getChannelName())
+        //#################################################
+        System.out.println("\n===유저 수정 테스트===\n");
+        user3.update("수정된 유저3", "수정된 이메일3","수정된 비번3");
+        userService.save(user3);
+        System.out.println("유저3 정보 수정 완료");
+        System.out.println(userService.read(user1.getId()));
+
+        //#################################################
+        System.out.println("\n===유저 삭제 테스트===\n");
+        userService.delete(user4.getId());
+        System.out.println("유저4 삭제 완료");
+        userService.readAll().stream()
                 .forEach(System.out::println);
 
-        channelService.addUserToChannel(user2.getId(), channel1.getId());
-        System.out.println("\n채널1 멤버 목록");
-        channel1.getMemberId().stream()
-                .map(userId -> userService.read(userId).getUserName())
-                .forEach(System.out::println);
+        //#################################################
+        System.out.println("\n===채널 등록 테스트===\n");
 
-        System.out.println("\n유저2 속한 채널");
-        user2.getJoinedChannelId().stream()
-                .map(channelId -> channelService.read(channelId).getChannelName())
-                .forEach(System.out::println);
+        Channel channel1 = new Channel("채널1", user1.getId());
+        channelService.create(channel1);
 
-//메시지 입력
-        Message message1 = new Message("메시지1 내용", channel1.getId(), user1.getId());
-        Message message2 = new Message("메시지2 내용", channel2.getId(), user1.getId());
-        Message message3 = new Message("메시지3 내용", channel1.getId(), user1.getId());
-        Message message4 = new Message("메시지4 내용", channel2.getId(), user2.getId());
+        channelService.addUserToChannel(user2.getId(),channel1.getId());
+        channelService.addUserToChannel(user3.getId(),channel1.getId());
+
+        //#################################################
+        System.out.println("\n===메세지 등록 테스트===\n");
+
+        Message message1 = new Message("유저1이 채널1에 작성한 메세지1", channel1.getId(), user1.getId());
+        Message message2 = new Message("유저2이 채널1에 작성한 메세지2", channel1.getId(), user2.getId());
+        Message message3 = new Message("유저3이 채널1에 작성한 메세지3", channel1.getId(), user3.getId());
 
         messageService.create(message1);
         messageService.create(message2);
         messageService.create(message3);
-        messageService.create(message4);
 
         for(Message message : messageService.readAll()){
             System.out.println(message.getContent());
         }
+        //#################################################
+        System.out.println("\n===메세지 작성한 유저 삭제 테스트===\n");
 
+        userService.delete(user3.getId());
+        System.out.println("유저3 삭제 완료");
+        userService.readAll().stream()
+                .forEach(System.out::println);
+        for(Message message : messageService.readAll()){
+            System.out.println(message.getContent());
+        }
+        //#################################################
+        System.out.println("\n===채널 어드민 유저 삭제 테스트===\n");
 
-        //메시지 수정
-        message1.update("메시지1 내용 수정1", channel1.getId(), user1.getId());
-        System.out.println(message1);
-//조회
-
-//유저1 삭제
         System.out.println("\n유저1 삭제");
         userService.delete(user1.getId());
 
@@ -125,5 +121,5 @@ public class JavaApplication {
         for(Message message : messageService.readAll()){
             System.out.println(message.getContent());
         }
-//삭제 확인(유저, 채널, 메시지)
-*/
+    }
+}
