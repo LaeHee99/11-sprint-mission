26111a255e4761c8ed665b97a65695ea3b3f6f4c
Laehee99 <gwangho1639@naver.com>
2026-03-30 11:46:30 +0900

    스프린트 미션 3 베이스코드 적용 및 오류 수정

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -4,60 +4,57 @@ import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
 import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.repository.ChannelRepository;
-import com.sprint.mission.discodeit.repository.MessageRepository;
-import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
-import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
-import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.basic.BasicChannelService;
-import com.sprint.mission.discodeit.service.basic.BasicMessageService;
-import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Optional;
 
 @SpringBootApplication
 public class DiscodeitApplication {
 	static User setupUser(UserService userService) {
 		UserCreateRequest request = new UserCreateRequest("woody", "woody@codeit.com", "woody1234");
-		User user = userService.create(request, Optional.empty());
-		return user;
+		return userService.create(request, Optional.empty());
 	}
 
 	static Channel setupChannel(ChannelService channelService) {
 		PublicChannelCreateRequest request = new PublicChannelCreateRequest("공지", "공지 채널입니다.");
-		Channel channel = channelService.create(request);
-		return channel;
+		return channelService.create(request);
 	}
 
 	static void messageCreateTest(MessageService messageService, Channel channel, User author) {
 		MessageCreateRequest request = new MessageCreateRequest("안녕하세요.", channel.getId(), author.getId());
-		Message message = messageService.create(request, new ArrayList<>());
+		// 빈 리스트를 더 안전하게 전달하기 위해 Collections.emptyList() 사용
+		Message message = messageService.create(request, Collections.emptyList());
 		System.out.println("메시지 생성: " + message.getId());
 	}
 
 	public static void main(String[] args) {
-
 		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+
 		// 서비스 초기화
 		UserService userService = context.getBean(UserService.class);
 		ChannelService channelService = context.getBean(ChannelService.class);
 		MessageService messageService = context.getBean(MessageService.class);
 
-		// 셋업
-		User user = setupUser(userService);
-		Channel channel = setupChannel(channelService);
-		// 테스트
-		messageCreateTest(messageService, channel, user);
+		// 셋업 및 테스트
+		try {
+			// 셋업
+			User user = setupUser(userService);
+			Channel channel = setupChannel(channelService);
+			// 테스트
+			messageCreateTest(messageService, channel, user);
+			System.out.println("테스트 데이터 세팅 성공");
+		} catch (IllegalArgumentException e) {
+			// 유저나 채널이 이미 존재해서 에러 날 시 그냥 넘어가기
+			System.out.println("이미 테스트 데이터가 존재합니다. : " + e.getMessage());
+		}
+
 	}
 }
